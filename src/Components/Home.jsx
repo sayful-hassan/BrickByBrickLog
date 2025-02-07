8340287b4c993498749309aba97e0f85d28b4e32
sayful-hassan <sayf.fashion@gmail.com>
2025-02-07 17:40:52 +0530

    Created the basic context needed in the home page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+function Home() {
+  return (
+    <div>
+      <h1>BRICK BY BRICK LOG</h1>
+      <p>Build your journey, one brick at a time</p>
+      <button>BRICK <strong>TRADE</strong> BRICK</button>
+    </div>
+  )
+}
+
+export default Home
