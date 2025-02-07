f8597121c2f305a00ca743398f01a09c95a5b7f1
sayful-hassan <sayf.fashion@gmail.com>
2025-02-07 17:56:02 +0530

    Implemented Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Components/Home'
 
 function App() {
 
   return (
-    <>
-    <Home/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom'
 import './Home.css'
 function Home() {
   return (
     <div>
       <h1>BRICK BY BRICK LOG</h1>
       <p>Build your journey, one brick at a time</p>
-      <button>BRICK <strong>TRADE</strong> BRICK</button>
+      <Link to='/brick-trade-brick'><button>BRICK <strong>TRADE</strong> BRICK</button></Link>
     </div>
   )
 }
